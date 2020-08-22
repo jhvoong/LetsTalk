e7c4711542066c65f50fd48ce80a5093ed2f6b5c
metaclips <utimichael9@gmail.com>
2020-08-22 02:48:42 +0100

    LoginPageUI: add loginpage screen

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container fill-height>
+    <div align="center">
+      <v-form>
+        <v-row justify="center" align="center">
+          <v-col cols="12">
+            <v-img src="../assets/unilag.svg" contain height="300" width="300"></v-img>
+          </v-col>
+
+          <v-col class="text-center" cols="12">
+            <span class="text-center mx-auto" v-if="isAdmin">Welcome Admin</span>
+            <span
+              class="text-center mx-auto"
+              v-else
+            >Welcome to the University of Lagos chatting platform</span>
+          </v-col>
+
+          <v-col cols="12" sm="6">
+            <v-text-field
+              v-model="email"
+              type="text"
+              :rules="emailRules"
+              name="email"
+              rounded
+              filled
+              required
+              placeholder="Email Address"
+            ></v-text-field>
+
+            <v-text-field
+              type="password"
+              v-model="password"
+              rounded
+              filled
+              required
+              name="password"
+              placeholder="Password"
+            ></v-text-field>
+
+            <v-alert v-if="signinErrorDetails" dense text type="error">{{signinErrorDetails}}</v-alert>
+          </v-col>
+          <v-col cols="12">
+            <v-btn tile>Log in</v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "LoginPage",
+
+  props: {
+    isAdmin: Boolean,
+  },
+
+  data: () => ({
+    signinErrorDetails: "",
+    email: "",
+    password: "",
+
+    emailRules: [
+      (v: string) => !!v || "E-mail is required",
+      (v: string) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+
+  methods: {},
+});
+</script>
+
+<style>
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../components/LoginPage.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Student Login',
+    component: Home,
+    props: { isAdmin: false }
   },
   {
     path: '/about',
